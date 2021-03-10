0234845f3aae9fb67b25144b3da405da056c9797
mycak <mycak@wp.pl>
2021-03-10 09:48:38 +0100

    server side graphql setup

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -7,6 +7,8 @@ import {
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
 import { Product } from './schemas/Products';
+import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL =
   process.env.DATABASE_URL || 'mongodb://localhost/keystone-toy-store';
@@ -36,10 +38,16 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
+      async onConnect(keystone) {
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       User,
       Product,
+      ProductImage,
     }),
     ui: {
       isAccessAllowed: ({ session }) => !!session?.data,
